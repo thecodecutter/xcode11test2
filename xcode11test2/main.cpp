250a905c8ffa8b5c8fafbae735dad531eb91a02f
Nick Lansbury <nick.lansbury@gmail.com>
2019-08-29 13:32:35 +0100

    Initial Commit

---
+++ xcode11test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode11test2
+//
+//  Created by Nick Lansbury on 29/08/2019.
+//  Copyright © 2019 ShinyObjects.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
