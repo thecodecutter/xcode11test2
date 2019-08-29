83e179c08532823fedeff421026763f92cedfdeb
Nick Lansbury <nick.lansbury@gmail.com>
2019-08-29 13:36:40 +0100

    Test code change using XCode 11.0 beta 6

--- xcode11test2/main.cpp
+++ xcode11test2/main.cpp
@@ -10,6 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello lovely C++ World! This was originally coded using XCode 10.3\n\n\n";
+    // Update 29/08/2019 I set the following to make this work in the XCode 11 beta
+    // Nicks-MacBook-Air:~ nicklansbury$ sudo xcode-select --switch '/Applications/Xcode-beta.app'
+    //
+    std::cout << "Hello lovely C++ World! This is now amended and testing in XCode 11.0 beta 6\n\n\n";
     return 0;
 }
+
